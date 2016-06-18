1a0caa51fe51e4f0162ad23398c8eae3c89cbb13
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-17 20:09:38 -0400

    Range constructors

---
+++ ranges.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+
+class Ranges < Minitest::Test
+  def test_inclusive_range
+    literal = 1..3
+    constructor = Range.new(1, 3)
+    assert_equal [1, 2, 3], literal.to_a
+    assert_equal [1, 2, 3], constructor.to_a
+  end
+
+  def test_exclusive_range
+    subject = 1...3
+    assert_equal [1, 2], subject.to_a
+  end
+
+  def test_alphabetical_range
+    literal = 'a'..'c'
+    constructor = Range.new('a', 'c')
+    assert_equal ['a', 'b', 'c'], literal.to_a
+    assert_equal ['a', 'b', 'c'], constructor.to_a
+  end
+end
